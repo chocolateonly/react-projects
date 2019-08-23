18fd94794ca610e8dec09e855ef50aa37bd3edfe
zhangqiao <1226937462@qq.com>
2019-08-23 17:21:51 +0800

    add page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Tabs, Card, Icon,Select} from 'antd';
+import {Tabs, Card, Icon, Select, Row, Col, Affix, Button} from 'antd';
 
 const {TabPane} = Tabs;
 const {Meta} = Card;
-const { Option } = Select;
+const {Option} = Select;
+const tabs = ['未', '已', '到期'];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [
-        {id: 1,houseType:'1.1', owner: 'hh', tel: '123342',address:'路',status:'空',rentOutStartTime:'',rentOutEndTime:''},
-        {id: 2,houseType:'1.2', owner: 'aahh', tel: '1233546542',address:'路',status:'空',rentOutStartTime:'',rentOutEndTime:''}
+        {
+          id: 1,
+          houseType: '1.1',
+          owner: 'hh',
+          tel: '123342',
+          address: '路',
+          status: '空',
+          rentOutStartTime: '',
+          rentOutEndTime: ''
+        },
+        {
+          id: 2,
+          houseType: '1.2',
+          owner: 'aahh',
+          tel: '1233546542',
+          address: '路',
+          status: '空',
+          rentOutStartTime: '',
+          rentOutEndTime: ''
+        },
+        {
+          id: 2,
+          houseType: '1.2',
+          owner: 'aahh',
+          tel: '1233546542',
+          address: '路',
+          status: '空',
+          rentOutStartTime: '',
+          rentOutEndTime: ''
+        },
+        {
+          id: 2,
+          houseType: '1.2',
+          owner: 'aahh',
+          tel: '1233546542',
+          address: '路',
+          status: '空',
+          rentOutStartTime: '',
+          rentOutEndTime: ''
+        }
       ],
       loading: false,
     }
@@ -33,56 +72,65 @@ class App extends Component {
   onSearch = () => {
 
   };
+
   render() {
     const {data} = this.state;
 
     return (
       <div className="App">
-        <Tabs defaultActiveKey="1" onChange={this.tabsCallback}>
-          <TabPane tab="Tab 1" key="1">
-
-            <Select
-              showSearch
-              style={{ width: '100%' }}
-              placeholder="Select a person"
-              optionFilterProp="children"
-              onChange={this.onChange}
-              onFocus={this.onFocus}
-              onBlur={this.onBlur}
-              onSearch={this.onSearch}
-              filterOption={(input, option) =>
-                option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-            >
-              <Option value="jack">Jack</Option>
-              <Option value="lucy">Lucy</Option>
-              <Option value="tom">Tom</Option>
-            </Select>
+        <Tabs defaultActiveKey="1" onChange={this.tabsCallback} style={{height: '100%'}}>
+          <TabPane tab={tabs[0]} key="1" style={{height: '100%'}}>
+            <div style={{display:'flex',flexDirection:'column',flex:1, overflow: 'scroll'}}>
 
 
-            {
-              data.map((item,index)=>{
-                return <Card
-                  key={'card'+index}
-                  className={'card'}
-                  style={{margin: '0 10px 10px 10px'}}
-                  actions={[<Icon type="edit"/>, <Icon type="ellipsis"/>]}
+              <div className={'top-tools'} style={{margin: '0 10px 10px 10px'}}>
+                <Select
+                  showSearch
+                  style={{width: '100%'}}
+                  placeholder="Select a person"
+                  optionFilterProp="children"
+                  onChange={this.onChange}
+                  onFocus={this.onFocus}
+                  onBlur={this.onBlur}
+                  onSearch={this.onSearch}
+                  filterOption={(input, option) =>
+                    option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                  }
                 >
-                  <Meta
-                    title={item.houseType}
-                    description={item.address}
-                  />
-                  <a href={`tel:${item.tel}`}>{item.tel}</a>
-                </Card>
-              })
-            }
+                  <Option value="jack">Jack</Option>
+                  <Option value="lucy">Lucy</Option>
+                  <Option value="tom">Tom</Option>
+                </Select>
+              </div>
 
+              {
+                data.map((item, index) => {
+                  return <Card
+                    key={'card' + index}
+                    className={'card'}
+                    style={{margin: '0 10px 10px 10px'}}
+                    actions={[<Icon type="edit"/>, <Icon type="ellipsis"/>]}
+                  >
+                    <Meta
+                      title={item.houseType}
+                      description={item.address}
+                    />
+                    <a href={`tel:${item.tel}`}>{item.tel}</a>
+                  </Card>
+                })
+              }
+
+            </div>
+
+            <Affix offsetBottom={20} onChange={affixed => console.log(affixed)}>
+              <Button>120px to affix top</Button>
+            </Affix>
 
           </TabPane>
-          <TabPane tab="Tab 2" key="2">
+          <TabPane tab={tabs[1]} key="2">
             Content of Tab Pane 2
           </TabPane>
-          <TabPane tab="Tab 3" key="3">
+          <TabPane tab={tabs[2]} key="3">
             Content of Tab Pane 3
           </TabPane>
         </Tabs>
